205cce632aedacb8292f9022053b571206ba778a
Linton Ye <linton@jimulabs.com>
2019-06-22 11:03:53 -0700

    Add style attrs

--- app/src/index.js
+++ app/src/index.js
@@ -5,8 +5,13 @@ import "./styles.css"
 
 function App() {
   return (
-    <div className="App">
-      <Frame />
+    <div
+      className="App"
+      style={{ width: 120, height: 120, background: "orange" }}
+    >
+      <Frame style={{ width: 120 }} height={60} borderRadius={30}>
+        <Frame size={60} borderRadius={30} />
+      </Frame>
     </div>
   )
 }
