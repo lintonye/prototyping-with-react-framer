0600dbe267322efad6d7b1fdcb1ffc163f5a0c51
Linton Ye <linton@jimulabs.com>
2019-06-25 10:50:11 -0700

    add updated code from 2.7

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Frame } from "framer"
+import { Frame, useCycle } from "framer"
 import "./styles.css"
 
 // parameter
@@ -24,11 +24,22 @@ console.log(microwave(sandwichMaker("🍤")))
 console.log(microwave(sandwichMaker("🍖")))
 
 function App() {
+  let [knobX, cycleKnobX] = useCycle(0, 60)
   return (
     <div className="App">
       <div>{sandwichMaker("🥓")}</div>
-      <Frame width={120} height={60} borderRadius={30}>
-        <Frame size={60} borderRadius={30} />
+      <Frame
+        width={120}
+        height={60}
+        borderRadius={30}
+        onTap={function handleTap() {
+          // change the value of knobX
+          console.log("tapped")
+          // knobX = 60 // won't work in React!
+          cycleKnobX()
+        }}
+      >
+        <Frame size={60} borderRadius={30} x={knobX} />
       </Frame>
     </div>
   )
