1474e5f4c41cbe1a30e98d1287337106ccc9cdf7
Linton Ye <linton@jimulabs.com>
2019-07-22 15:59:34 -0700

    Add 4.1 changes

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,33 +1,45 @@
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { Frame } from "framer"
+import bgSvg from "./bg.svg"
+import sunSvg from "./sun.svg"
+import cloudsSvg from "./clouds.svg"
+import birdSvg from "./bird.svg"
 
 import "./styles.css"
 
 function App() {
   return (
     <div className="App">
-      <Frame size={600} position="relative">
+      <Frame size={600} center background={null}>
         <Frame
           // bg
           size={500}
           top={50}
           left={20}
+          image={bgSvg}
+          background={null}
         />
         <Frame
           // sun
           left={155}
           top={15}
+          image={sunSvg}
+          background={null}
         />
         <Frame
           // cloud
           left={335}
           top={55}
+          image={cloudsSvg}
+          background={null}
         />
         <Frame
           // bird
           left={35}
           top={200}
+          image={birdSvg}
+          background={null}
         />
       </Frame>
     </div>
