5b0420736b0f54f46283fddb2c348ef689f29189
Linton Ye <linton@jimulabs.com>
2019-07-25 16:08:45 -0700

    Add starter code

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,68 +4,27 @@ import { Frame, useAnimation } from "framer"
 
 import "./styles.css"
 
+const style = {
+  backgroundImage:
+    "url(https://cdn.glitch.com/071e5391-90f7-476b-b96c-1f51f7106b0c%2Fbird_strong_small.svg?v=1560032432704)",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "contain",
+  backgroundPosition: "center",
+  backgroundColor: "#55CCFF",
+  boxShadow: "2px 2px 10px 0 rgba(0,0,0,0.25)"
+}
+
 function App() {
-  const birdAnim = useAnimation()
-  const cloudsAnim = useAnimation()
-  const sunAnim = useAnimation()
-  const bgAnim = useAnimation()
   return (
     <div className="App">
       <Frame
-        size={600}
         center
-        background={null}
-        onMouseMove={function(e) {
-          // 1. Find mouse position
-          let offsetXFromCenter = e.clientX - window.innerWidth / 2
-          let offsetYFromCenter = e.clientY - window.innerHeight / 2
-
-          birdAnim.start({ x: offsetXFromCenter / 6, y: offsetYFromCenter / 6 })
-          cloudsAnim.start({
-            x: offsetXFromCenter / 8,
-            y: offsetYFromCenter / 8
-          })
-          sunAnim.start({
-            x: offsetXFromCenter / 10,
-            y: offsetYFromCenter / 10
-          })
-          bgAnim.start({ x: offsetXFromCenter / 12, y: offsetYFromCenter / 12 })
-        }}
-      >
-        <Frame
-          // bg
-          size={500}
-          top={50}
-          left={20}
-          background={null}
-          image="https://image.flaticon.com/icons/svg/119/119596.svg"
-          animate={bgAnim}
-        />
-        <Frame
-          // sun
-          left={155}
-          top={15}
-          background={null}
-          image="https://image.flaticon.com/icons/svg/789/789395.svg"
-          animate={sunAnim}
-        />
-        <Frame
-          // cloud
-          left={335}
-          top={55}
-          image="https://image.flaticon.com/icons/svg/414/414927.svg"
-          background={null}
-          animate={cloudsAnim}
-        />
-        <Frame
-          // bird
-          left={35}
-          top={200}
-          image="https://image.flaticon.com/icons/svg/789/789392.svg"
-          background={null}
-          animate={birdAnim}
-        />
-      </Frame>
+        drag="x"
+        height={300}
+        style={style}
+        dragConstraints={{ left: -200, right: 200 }}
+        borderRadius={10}
+      />
     </div>
   )
 }
