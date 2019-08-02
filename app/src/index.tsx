8297d8b2a58cac96b3a648b2c54a2e4541f94e83
Linton Ye <linton@jimulabs.com>
2019-08-02 10:41:17 -0700

    Add toggle code

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,73 +1,39 @@
 import * as React from "react"
 import { render } from "react-dom"
-
 import "./styles.css"
-import { Frame } from "framer"
-
-function Radio(props) {
-  return (
-    <Frame
-      position="relative"
-      size="auto"
-      style={{ display: "flex", marginBottom: 10 }}
-      background="null"
-      {...props}
-    >
-      <Frame
-        size={30}
-        shadow="0 1px 5px 0 rgba(0,0,0,0.3)"
-        background="white"
-        borderRadius={15}
-        position="relative"
-        style={{ marginRight: 10 }}
-      >
-        <Frame
-          size={20}
-          borderRadius={10}
-          background="purple"
-          center
-          animate={{ scale: props.selected ? 1 : 0 }}
-        />
-      </Frame>
-      {props.children}
-    </Frame>
-  )
-}
-
-// DRY = Do not Repeat Yourself
-function RadioGroup({ choices, ...props }) {
-  let [selectedIndex, setSelectedIndex] = React.useState(0)
-  return (
-    <Frame background={null} {...props}>
-      {choices.map((choice, index) => (
-        <Radio
-          selected={index === selectedIndex}
-          key={choice}
-          onClick={function() {
-            // change selectedIndex
-            setSelectedIndex(index)
-          }}
-        >
-          {choice}
-        </Radio>
-      ))}
-    </Frame>
-  )
-}
+import { Frame, useCycle } from "framer"
 
 function App() {
-  let options = [
-    "🍏 Apple",
-    "🥑 Avocado",
-    "🌶 Pepper",
-    "🥕 Carrot",
-    "🍔 Burger",
-    "🥝 Kiwi fruit",
-    "🍌 Banana"
-  ]
+  let [mode, cycleMode] = useCycle("off", "on")
   return (
     <div className="App">
-      <RadioGroup center size="auto" choices={options} />
+      <Frame
+        width={120}
+        height={60}
+        borderRadius={30}
+        center
+        onTap={function handleTap() {
+          cycleMode()
+        }}
+        background="#999"
+        animate={mode}
+        variants={{
+          off: { background: "#999" },
+          on: { background: "orange" }
+        }}
+      >
+        <Frame
+          size={60}
+          borderRadius={30}
+          variants={{
+            off: { x: 0, scale: 1 },
+            on: { x: 60, scale: 1.2 }
+          }}
+          transition={{ duration: 0.2 }}
+          background="white"
+          shadow="0 1px 5px rgba(0,0,0,0.5)"
+        />
+      </Frame>
     </div>
   )
 }
