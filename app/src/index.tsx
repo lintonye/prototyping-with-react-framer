094456ae19709afb7edab861e5268a32af2720fe
Linton Ye <linton@jimulabs.com>
2019-08-03 08:21:30 -0700

    cycleX

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,18 +3,8 @@ import { render } from "react-dom"
 import "./styles.css"
 import { Frame, useCycle } from "framer"
 
-function useCycle2(options) {
-  let [index, setIndex] = React.useState(0)
-  let currentOption = options[index]
-  function cycle() {
-    setIndex(index < options.length - 1 ? index + 1 : 0)
-  }
-  return [currentOption, cycle]
-}
-
 function App() {
-  let [mode, cycleMode] = useCycle2(["off", "on"])
-  console.log("new page, mode=", mode)
+  let [xOffset, cycleX] = useCycle(0, 60)
   return (
     <div className="App">
       <Frame
@@ -23,22 +13,14 @@ function App() {
         borderRadius={30}
         center
         onTap={function handleTap() {
-          cycleMode()
+          cycleX()
         }}
         background="#999"
-        animate={mode}
-        variants={{
-          off: { background: "#999" },
-          on: { background: "orange" }
-        }}
       >
         <Frame
           size={60}
           borderRadius={30}
-          variants={{
-            off: { x: 0, scale: 1 },
-            on: { x: 60, scale: 1.2 }
-          }}
+          animate={{ x: xOffset }}
           transition={{ duration: 0.2 }}
           background="white"
           shadow="0 1px 5px rgba(0,0,0,0.5)"
