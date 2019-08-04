8e8eb44a2c2ead1d75b0aab952182be78a5680aa
Linton Ye <linton@jimulabs.com>
2019-08-04 14:34:17 -0700

    Add updates from 4.3

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,20 +1,30 @@
 import * as React from "react"
 import ReactDOM from "react-dom"
-import { Frame } from "framer"
+import { Frame, useAnimation } from "framer"
 
 import "./styles.css"
 
 function App() {
+  let birdAnim = useAnimation()
+  let cloudsAnim = useAnimation()
+  let sunAnim = useAnimation()
+  let bgAnim = useAnimation()
+
   return (
     <div className="App">
       <Frame
         size={600}
-        center
         background={null}
-        onMouseMove={function(e) {
+        center
+        onMouseMove={function(event) {
+          let offsetX = event.clientX - window.innerWidth / 2
+          let offsetY = event.clientY - window.innerHeight / 2
           // 1. Find mouse position
-          console.log(e.clientX, e.clientY)
           // 2. create animations
+          birdAnim.start({ x: offsetX / 6, y: offsetY / 6 })
+          cloudsAnim.start({ x: offsetX / 8, y: offsetY / 8 })
+          sunAnim.start({ x: offsetX / 10, y: offsetY / 10 })
+          bgAnim.start({ x: offsetX / 14, y: offsetY / 14 })
         }}
       >
         <Frame
@@ -24,6 +34,7 @@ function App() {
           left={20}
           background={null}
           image="https://image.flaticon.com/icons/svg/119/119596.svg"
+          animate={bgAnim}
         />
         <Frame
           // sun
@@ -31,20 +42,23 @@ function App() {
           top={15}
           background={null}
           image="https://image.flaticon.com/icons/svg/789/789395.svg"
+          animate={sunAnim}
         />
         <Frame
           // cloud
+          background={null}
           left={335}
           top={55}
           image="https://image.flaticon.com/icons/svg/414/414927.svg"
-          background={null}
+          animate={cloudsAnim}
         />
         <Frame
           // bird
+          background={null}
           left={35}
           top={200}
           image="https://image.flaticon.com/icons/svg/789/789392.svg"
-          background={null}
+          animate={birdAnim}
         />
       </Frame>
     </div>
