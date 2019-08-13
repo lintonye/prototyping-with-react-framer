3d50cb24c5c1a8556d24b5d798b1eb7420b6ca90
Linton Ye <linton@jimulabs.com>
2019-08-13 12:46:37 -0700

    Add padlock code

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,84 +4,60 @@ import { Frame, transform, useAnimation } from "framer"
 
 import "./styles.css"
 
-function Skinny(props) {
+function Shackle() {
   return (
-    <Frame
-      width={290}
-      height={320}
-      position="relative"
-      background="transparent"
-    >
-      <Frame
-        background="url(https://cdn.glitch.com/071e5391-90f7-476b-b96c-1f51f7106b0c%2Fskinny-portrait.png)"
-        width={290}
-        height={290}
-        borderRadius={150}
+    <svg xmlns="http://www.w3.org/2000/svg" width="87" height="109">
+      <path
+        d="M 5.438 63.942 L 5.438 43.91 C 5.438 22.888 22.479 5.847 43.5 5.847 L 43.5 5.847 C 64.521 5.847 81.563 22.888 81.563 43.91 L 81.563 103.947"
+        fill="transparent"
+        strokeWidth="9.97"
+        stroke="#fff"
+        strokeLinecap="round"
       />
-      {/* Cheek */}
-      <Frame
-        background="url(https://cdn.glitch.com/071e5391-90f7-476b-b96c-1f51f7106b0c%2Fcheek.png)"
-        width={79}
-        height={67}
-        left={155}
-        top={135}
-        // props = { cheekAnimate: ... }
-        animate={props.cheekAnimate}
-      />
-    </Frame>
+    </svg>
   )
 }
 
-function Slider({ onSlide }) {
+function Body() {
   return (
-    <Frame
-      width={280}
-      height={15}
-      borderRadius={30}
-      backgroundColor="white"
-      position="relative"
-    >
-      <Frame
-        drag={"x"}
-        dragConstraints={{ left: 0, right: 250 }}
-        dragElastic={false}
-        dragMomentum={false}
-        onDrag={onSlide}
-        size={60}
-        borderRadius={30}
-        center="y"
-        backgroundColor="white"
-        shadow="0 1px 5px 0 rgba(0, 0, 0, 0.25)"
+    <svg xmlns="http://www.w3.org/2000/svg" width="101" height="99">
+      <path
+        d="M 0 19 C 0 8.507 8.507 0 19 0 L 82 0 C 92.493 0 101 8.507 101 19 L 101 80 C 101 90.493 92.493 99 82 99 L 19 99 C 8.507 99 0 90.493 0 80 Z"
+        fill="#fff"
       />
+    </svg>
+  )
+}
+
+function Padlock() {
+  // const [locked, setLocked] = useState(true)
+  const controls = useAnimation()
+  let locked = true
+  async function toggleLock() {
+    if (locked) {
+      await controls.start({ rotateY: 0 })
+      await controls.start({ y: 0 })
+    } else {
+      await controls.start({ y: -30 })
+      await controls.start({ rotateY: 180 })
+    }
+    // setLocked(!locked)
+    locked = !locked
+  }
+  return (
+    <Frame center background={null} width={100} onTap={toggleLock}>
+      <Frame background={null} left={5} animate={controls} originX={0.41}>
+        <Shackle />
+      </Frame>
+      <Frame background={null} top={60}>
+        <Body />
+      </Frame>
     </Frame>
   )
 }
 
 function App() {
-  let animationControls = useAnimation()
-  function handleDrag(event, info) {
-    // change the scale of Skinny’s cheek according to the position of slider knob
-    let newScale = transform(info.point.x, [0, 250], [0.4, 1.5])
-    animationControls.start({
-      scale: newScale,
-      transition: { type: "spring", velocity: 0 }
-    })
-  }
-  return (
-    <div
-      className="App"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column"
-      }}
-    >
-      <Skinny cheekAnimate={animationControls} />
-      <Slider onSlide={handleDrag} />
-    </div>
-  )
+  return <Padlock />
 }
 
 const rootElement = document.getElementById("root")
