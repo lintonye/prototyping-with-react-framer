3e204c564d9ec805b59377c472012490a2e3d0bd
Linton Ye <linton@jimulabs.com>
2019-08-17 08:52:46 -0700

    add updates from screencast

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,43 @@
 import * as React from "react"
 import { render } from "react-dom"
-import { Frame, Scroll, useMotionValue, useTransform } from "framer"
+import {
+  Frame,
+  Scroll,
+  useMotionValue,
+  useTransform,
+  useAnimation
+} from "framer"
+import {
+  usePositiveOffset,
+  useSticky,
+  useSpeed,
+  useTrigger
+} from "use-parallax"
 
 import "./styles.css"
 
+function usePosOffset(offset) {
+  return useTransform(offset, v => -v)
+}
+
 function App() {
   const offset = useMotionValue(0)
+  const pOffset = usePositiveOffset(offset)
   const iconOpacity = useTransform(offset, [0, -500], [1, 0])
-  const iconBorderRadius = useTransform(offset, [0, -500], [20, 100])
+  const iconBorderRadius = useTransform(pOffset, [0, 500], [20, 100])
+  const iconY = useSpeed(
+    pOffset,
+    [0, 100],
+    0,
+    [100, 400],
+    -1 / 2,
+    [400, 800],
+    -1
+  )
+  const anim = useAnimation()
+  useTrigger(pOffset, [400, 450], direction => {
+    anim.start({ rotate: direction < 0 ? 180 : 0 })
+  })
   // offset.onChange(v => console.log(v))
   return (
     <Scroll width="100%" height="100%" contentOffsetY={offset}>
@@ -21,7 +51,9 @@ function App() {
         />
         {/* Icon */}
         <Frame
-          opacity={iconOpacity}
+          // opacity={iconOpacity}
+          y={iconY}
+          animate={anim}
           backgroundColor="white"
           size={200}
           top={400}
