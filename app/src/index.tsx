ad40551c20e26b1bfde56a4b999d14dc165b38ff
Linton Ye <linton@jimulabs.com>
2019-08-16 18:18:07 -0700

    Add updates from 8.1

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,29 +1,36 @@
 import * as React from "react"
 import { render } from "react-dom"
-import { Frame } from "framer"
+import { Frame, Scroll, useMotionValue, useTransform } from "framer"
 
 import "./styles.css"
 
 function App() {
+  const offset = useMotionValue(0)
+  const iconOpacity = useTransform(offset, [0, -500], [1, 0])
+  const iconBorderRadius = useTransform(offset, [0, -500], [20, 100])
+  // offset.onChange(v => console.log(v))
   return (
-    <Frame background={null}>
-      {/* Plaid background */}
-      <Frame
-        height={2000}
-        width="100%"
-        opacity={0.5}
-        background="url(/plaid.jpeg)"
-      />
-      {/* Icon */}
-      <Frame
-        backgroundColor="white"
-        size={200}
-        top={400}
-        borderRadius={20}
-        center="x"
-        image="/starship.png"
-      />
-    </Frame>
+    <Scroll width="100%" height="100%" contentOffsetY={offset}>
+      <Frame background={null}>
+        {/* Plaid background */}
+        <Frame
+          height={2000}
+          width="100%"
+          opacity={0.5}
+          background="url(/plaid.jpeg)"
+        />
+        {/* Icon */}
+        <Frame
+          opacity={iconOpacity}
+          backgroundColor="white"
+          size={200}
+          top={400}
+          borderRadius={iconBorderRadius}
+          center="x"
+          image="/starship.png"
+        />
+      </Frame>
+    </Scroll>
   )
 }
 
