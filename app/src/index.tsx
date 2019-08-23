ba81c5839bbbeb59cba28e829b087c15613555c2
Linton Ye <linton@jimulabs.com>
2019-08-23 11:44:50 -0700

    Add 9.3 changes

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
 import { render } from "react-dom"
-import { Frame } from "framer"
+import { Frame, useCycle } from "framer"
 
 import { PhoneScreen } from "./PhoneScreen"
 import "./styles.css"
 
 function App() {
+  const [mode, cycleMode] = useCycle("list", "detail")
   return (
     <div className="App">
       <PhoneScreen background="white">
@@ -17,14 +18,30 @@ function App() {
           height={300}
           image="/hotel1.jpg"
           borderRadius={16}
+          onTap={() => cycleMode()}
+          animate={mode}
+          variants={{
+            list: { left: 16, right: 16, y: 0, borderRadius: 16 },
+            detail: { left: 0, right: 0, y: -80, borderRadius: 0 }
+          }}
         />
         {/* Description */}
         <Frame
           left={16}
           right={16}
           top={380}
+          animate={mode}
           height={347}
           image="/description.png"
+          variants={{
+            list: { height: 0, y: 0, left: 16, right: 16 },
+            detail: {
+              height: 347,
+              y: -80,
+              left: 8,
+              right: 8
+            }
+          }}
         />
       </PhoneScreen>
     </div>
