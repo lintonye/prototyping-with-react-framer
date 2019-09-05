256296d695663a918375f392de2c8b10e6e7f6d8
Linton Ye <linton@jimulabs.com>
2019-09-04 17:57:31 -0700

    Add updates from 7.5

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,17 +1,54 @@
 import * as React from "react"
 import { render } from "react-dom"
-import { Frame, transform, useCycle } from "framer"
+import { Frame, useCycle } from "framer"
 
 import "./styles.css"
 
 function Fab({ buttons, ...props }) {
   const [mode, cycleMode] = useCycle("folded", "expanded")
   return (
-    <Frame background={null} size="auto" animate={mode} {...props}>
+    <Frame
+      size="auto"
+      background={null}
+      initial="folded"
+      animate={mode}
+      {...props}
+    >
+      <Frame
+        background={null}
+        size="auto"
+        center="x"
+        position="relative"
+        variants={{
+          folded: { transition: { staggerChildren: 0.05 } },
+          expanded: {
+            transition: { staggerChildren: 0.05, staggerDirection: -1 }
+          }
+        }}
+      >
+        {buttons.map(button => (
+          <Frame
+            size={40}
+            key={button.image}
+            borderRadius="50%"
+            position="relative"
+            image={button.image}
+            style={{ marginBottom: 16 }}
+            center="x"
+            border="2px solid white"
+            shadow="1px 1px 5px rgba(0,0,0,0.5)"
+            variants={{
+              folded: { scale: 0 },
+              expanded: { scale: 1 }
+            }}
+          />
+        ))}
+      </Frame>
       {/* FAB */}
       <Frame
         size={60}
         borderRadius="50%"
+        position="relative"
         backgroundColor="white"
         shadow="1px 1px 5px rgba(0,0,0,0.5)"
         onTap={function() {
@@ -21,11 +58,12 @@ function Fab({ buttons, ...props }) {
         <Frame
           size={30}
           background={null}
+          center
+          image="/share.svg"
           variants={{
             folded: { image: "/share.svg", rotate: 0, x: -2, y: 1 },
             expanded: { image: "/x.svg", rotate: 180, x: 0, y: 0 }
           }}
-          center
         />
       </Frame>
     </Frame>
