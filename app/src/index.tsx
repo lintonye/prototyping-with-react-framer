bfeb46c606c03642822da53f3ad45ec4ffc1387d
Linton Ye <linton@jimulabs.com>
2019-09-07 06:40:08 -0700

    Add toggle code

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,19 +1,49 @@
-import * as React from "react"
-import { render } from "react-dom"
-
+import React from "react"
+import ReactDOM from "react-dom"
+import { Frame, useCycle } from "framer"
 import "./styles.css"
-import { motion } from "framer-motion"
 
 function App() {
+  let [mode, cycleMode] = useCycle("off", "on")
+  // let result = useCycle(0, 60)
+  // let knobX = result[0]
+  // let cycleKnobX = result[1]
   return (
     <div className="App">
-      <motion.div
-        style={{ width: 200, height: 200, background: "orange" }}
-        animate={{ scale: 2 }}
-      />
+      <Frame
+        width={120}
+        height={60}
+        borderRadius={30}
+        center
+        onTap={function handleTap() {
+          // change the value of knobX
+          console.log("tapped")
+          // knobX = 60 // won't work in React!
+          cycleMode()
+        }}
+        background="#999"
+        animate={mode}
+        variants={{
+          off: { background: "#999" },
+          on: { background: "orange" }
+        }}
+      >
+        <Frame
+          size={60}
+          borderRadius={30}
+          // animate={mode}
+          variants={{
+            off: { x: 0, scale: 1 },
+            on: { x: 60, scale: 1.2 }
+          }}
+          transition={{ duration: 0.2 }}
+          background="white"
+          shadow="0 1px 5px rgba(0,0,0,0.5)"
+        />
+      </Frame>
     </div>
   )
 }
 
 const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+ReactDOM.render(<App />, rootElement)
