6094ba5beeb3ccac4358383c1fb20bcb0a74b458
Linton Ye <linton@jimulabs.com>
2019-09-11 12:49:11 -0700

    Update styles

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,27 +6,19 @@ import "./styles.css"
 
 function App() {
   return (
-    <motion.div>
-      <motion.div>
-        {/* Plaid background */}
-        <motion.div
-          style={{
-            height: 2000,
-            width: "100%",
-            opacity: 0.5,
-            background: "url(/plaid.jpeg)",
-            position: "absolute",
-            top: 0
-          }}
-        />
-      </motion.div>
+    <motion.div
+      style={{
+        height: 2000,
+        width: "100%",
+        background: "url(/plaid.jpeg)"
+      }}
+    >
       {/* Icon */}
       <motion.img
         src="/starship.png"
         style={{
           width: 200,
           background: "white",
-          z: 1,
           margin: "400px 0 0 200px"
         }}
       />
