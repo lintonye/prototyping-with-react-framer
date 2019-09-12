57fd788e8f0b33d51f1b7fa88588f1b13af31a8a
Linton Ye <linton@jimulabs.com>
2019-09-12 12:54:05 -0700

    Add end result

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,41 +1,80 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import * as React from "react"
+import { render } from "react-dom"
 import { motion, useMotionValue, useTransform } from "framer-motion"
+
 import "./styles.css"
 
-function CircularProgress({ progress }) {
-  return (
-    <motion.svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="258"
-      height="258"
-      viewBox="0 0 258 258"
-    >
-      {/* Check mark  */}
-      <motion.path
-        transform="translate(60 85)"
-        d="M 46.477 100.11 L 0 53.633 L 5.931 47.703 L 46.477 88.249 L 134.729 0 L 140.66 5.93 Z"
-        fill="#66BB66"
-      />
-      {/* Circle */}
-      <motion.path
-        d="M 130 6 C 198.483 6 254 61.517 254 130 C 254 198.483 198.483 254 130 254 C 61.517 254 6 198.483 6 130 C 6 61.517 61.517 6 130 6 Z"
-        fill="transparent"
-        strokeWidth="8"
-        stroke="#FFCC66"
-      />
-    </motion.svg>
-  )
-}
-
 function App() {
-  let progress = useMotionValue(0)
+  const initialAngle = (-90 / 180) * Math.PI
+  const angleMv = useMotionValue(0)
+  const trackR = 233 / 2
+  const strokeWidth = 19
+  const knobR = trackR - strokeWidth / 2
+  const knobSize = 18
+  const x = useTransform(angleMv, a => knobR * Math.cos(a + initialAngle))
+  const y = useTransform(angleMv, a => knobR * Math.sin(a + initialAngle))
+  const pathLength = useTransform(angleMv, [0, 2 * Math.PI], [0, 1])
   return (
     <div className="App">
-      <CircularProgress progress={progress} />
+      <motion.div
+        style={{
+          width: trackR * 2,
+          height: trackR * 2,
+          position: "relative"
+        }}
+        onPan={(_, info) => {
+          const { point } = info
+          // TODO this `center` may not be screen center
+          const center = [window.innerWidth / 2, window.innerHeight / 2]
+          const x = point.x - center[0]
+          const y = point.y - center[1]
+          let angle = Math.atan2(y, x) - initialAngle
+          if (angle < 0) angle += 2 * Math.PI
+
+          angleMv.set(angle)
+        }}
+      >
+        <motion.svg
+          width="233"
+          height="233"
+          viewBox="0 0 233 233"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%" stopColor="#05a" />
+              <stop offset="100%" stopColor="#0a5" />
+            </linearGradient>
+          </defs>
+          <motion.path
+            fill="transparent"
+            stroke="url(#linear)"
+            strokeWidth={strokeWidth}
+            strokeLinecap="round"
+            d="M116.5 10C175.318 10 223 57.682 223 116.5S175.318 223 116.5 223 10 175.318 10 116.5 57.682 10 116.5 10z"
+            style={{ pathLength }}
+          />
+        </motion.svg>
+        <motion.div
+          style={{
+            x,
+            y,
+            width: knobSize * 2,
+            height: knobSize * 2,
+            background: "white",
+            border: "1px solid rgba(0,0,0,.1)",
+            boxShadow: "1px 1px 4px rgba(0,0,0,.25)",
+            borderRadius: "50%",
+            left: trackR - knobSize,
+            top: trackR - knobSize,
+            position: "absolute"
+          }}
+        />
+      </motion.div>
     </div>
   )
 }
 
 const rootElement = document.getElementById("root")
-ReactDOM.render(<App />, rootElement)
+render(<App />, rootElement)
